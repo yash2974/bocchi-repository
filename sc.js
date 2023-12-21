2c89ce14eab16ab8c30e0c88dcf5b52b89b1a4bd
yash2974 <yashbisht2004@gmail.com>
2023-12-21 21:54:55 +0530

    1ver

--- sc.js
+++ sc.js
@@ -5,11 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
         'ryo (2).png',
         'ryo (3).png'
     ];
+    const nijika_array = [
+        'nijika (1).png',
+        'nijika (2).png',
+        'nijika (3).png'
+    ];
+    const bocchi_array = [
+        'bocchi (1).png',
+        'bocchi (2).png',
+        'bocchi (3).png'
+
+    ];
 
     // Get a random index
     let randomIndex = Math.floor(Math.random() * yamada_array.length);
 
     // Set the source of the element with id 'ryo-random'
     document.getElementById('ryo-random').src = `./ryo-array/${yamada_array[randomIndex]}`;
+    document.getElementById('nijika-random').src = `./nijika-array/${nijika_array[randomIndex]}`;
+    document.getElementById('bocchi-random').src = `./bocchi-array/${bocchi_array[randomIndex]}`;
+    
     
 });
