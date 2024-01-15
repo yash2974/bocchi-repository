207883423c039cad900fe5de6c92b2da324ad067
yash2974 <yashbisht2004@gmail.com>
2024-01-15 12:45:44 +0530

    manage

--- sc.js
+++ sc.js
@@ -1,25 +1,25 @@
 const randomFeature = [
     {
         name:"ryo",
-        randomImage:['ryo (2).png','ryo (2).png','ryo (2).png'],
+        randomImage:['ryo (1).png','ryo (2).png','ryo (3).png'],
         randomVideo:['ryoVid (1).webm','ryoVid (2).webm','ryoVid (3).webm','ryoVid (4).webm'],
         popup:"ryo-popup"
     },
     {
         name:"nijika",
-        randomImage:['nijika (1).png','nijika (1).png','nijika (1).png'],
+        randomImage:['nijika (1).png','nijika (2).png','nijika (3).png'],
         randomVideo:['nijVid (1).webm','nijVid (2).webm'],
         popup:"ryo-popup"
     },
     {
         name:"bocchi",
-        randomImage:['bocchi (1).png','bocchi (1).png','bocchi (1).png'],
+        randomImage:['bocchi (1).png','bocchi (2).png','bocchi (3).png'],
         randomVideo:['bocVid (1).webm','bocVid (2).webm','bocVid (3).webm','bocVid (4).webm','bocVid (5).webm','bocVid (6).webm','bocVid (7).webm'],
         popup:"ryo-popup"
     },
     {
         name:"kita",
-        randomImage:['kita (3).png','kita (3).png','kita (3).png'],
+        randomImage:['kita (1).png','kita (2).png','kita (3).png'],
         randomVideo:['kitVid (1).webm'],
         popup:"ryo-popup"
     }
